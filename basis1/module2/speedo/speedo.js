f785bd775d683feaf9d776575551e893f5a082df
87243 <87243@glr.nl>
2021-11-04 16:38:55 +0100

    module2 speedo made best

--- basis1/module2/speedo/speedo.js
+++ basis1/module2/speedo/speedo.js
@@ -1,12 +1,29 @@
-function function1(){
+function function1() {
     let naam = document.getElementById("nm").value;
-    let leeftijd = document.getElementById("ld").value;
-    document.getElementById("result").innerHTML = "Jouw naam is "+naam+" je bent "+leeftijd+" jaar oud.";
-    if (leeftijd >= 13 && leeftijd < 20){
-        document.getElementById("result2").innerHTML = "Je bent een tiener."
+    if (naam == ""){
+        document.getElementById("result").innerHTML = "";
     }
     else{
-        document.getElementById("result2").innerHTML = "Je bent geen tiener."
+    document.getElementById("result").innerHTML = "Jouw naam is " + naam;
+    }
+}
+
+function function2() {
+    let leeftijd = document.getElementById("ld").valueAsNumber;
+    if (!isNaN(leeftijd)){
+        document.getElementById("result2").innerHTML = " je bent " + leeftijd + " jaar oud.";
+    }
+    else{
+        document.getElementById("result2").innerHTML = "";
     }
 
+    if (leeftijd >= 13 && leeftijd < 20){
+        document.getElementById("result3").innerHTML = "Je bent een tiener."
+    }
+    else if (isNaN(leeftijd)){
+        document.getElementById("result3").innerHTML = "";
+    }
+    else{
+        document.getElementById("result3").innerHTML = "Je bent geen tiener."
+    }
 }
