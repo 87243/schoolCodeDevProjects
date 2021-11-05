b540b3b9b06316c2495782afc57040c4f4d659a8
87243 <87243@glr.nl>
2021-11-05 10:44:32 +0100

    update

--- basis1/module2/eindopdracht2/script.js
+++ basis1/module2/eindopdracht2/script.js
@@ -1,12 +1,21 @@
 let moneyJS = 330000000;
 let ministeriesJS = "";
+let inputId = 0;
 document.getElementById("money").innerHTML = "Jouw aantal geld over: "+moneyJS;
 console.log(moneyJS);
 
 function addMinisterieBJS() {
     let addMinisterieIJS = document.getElementById("addMinisterieI").value;
     console.log("a: "+addMinisterieIJS);
-    ministeriesJS = ministeriesJS+"<p>"+addMinisterieIJS+"</p><input id='' type='text'><br>";
-    document.getElementById("ministeries").innerHTML = ministeriesJS;
+
+    inputId = inputId+1;
+    console.log("inputId: "+inputId);
+
+    ministeriesJS = ministeriesJS+"<p>"+addMinisterieIJS+"</p><input id='"+"input"+inputId+"' type='text'><br>";
+    document.getElementById("ministeries").innerHTML = ministeriesJS+"<br><br><button id='sendMoneyB' onclick='sendMoneyB()'>Stuur geld</button>";
     console.log("b: "+ministeriesJS);
+}
+
+function sendMoneyB(){
+    
 }
