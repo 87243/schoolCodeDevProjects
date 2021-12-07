a233808ba36bcbc60046ea85bbc4a210f202a8fc
87243 <87243@glr.nl>
2021-12-07 17:02:39 +0100

    mediamt1 wintergames update

--- media/wintergames/script.js
+++ media/wintergames/script.js
@@ -1,8 +1,8 @@
 function setup() {
     createCanvas(500, 500);
-  }
+}
 
-  function draw() {
+function draw() {
     background(0);
     push();
     // fill(255, 204, 0);
@@ -12,23 +12,23 @@ function setup() {
     rotate(frameCount / mouseX);
     star(0, 0, 30, 70, 5);
     pop();
-  }
+}
 
-  function mouseMoved() {
+function mouseMoved() {
     // console.log(mouseX, mouseY);
-  }
+}
 
-  function star(x, y, radius1, radius2, npoints) {
+function star(x, y, radius1, radius2, npoints) {
     let angle = TWO_PI / npoints;
     let halfAngle = angle / 2.0;
     beginShape();
     for (let a = 0; a < TWO_PI; a += angle) {
-      let sx = x + cos(a) * radius2;
-      let sy = y + sin(a) * radius2;
-      vertex(sx, sy);
-      sx = x + cos(a + halfAngle) * radius1;
-      sy = y + sin(a + halfAngle) * radius1;
-      vertex(sx, sy);
+        let sx = x + cos(a) * radius2;
+        let sy = y + sin(a) * radius2;
+        vertex(sx, sy);
+        sx = x + cos(a + halfAngle) * radius1;
+        sy = y + sin(a + halfAngle) * radius1;
+        vertex(sx, sy);
     }
     endShape(CLOSE);
-  }
+}
