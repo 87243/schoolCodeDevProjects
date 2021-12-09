6fd85585ae92739147e9add307f12a6fdeb27607
87243 <87243@glr.nl>
2021-12-09 13:57:03 +0100

    basis1 module4 update

--- basis1/module4/opdracht4/js/script.js
+++ basis1/module4/opdracht4/js/script.js
@@ -1,5 +1,14 @@
 let amountOfDivs = 10;
-for(let i = 0; i <= amountOfDivs; i++){
-    // document.getElementById("div1").innerHTML +=
-    document.body.innerHTML += `<div id="divInsideDiv${i}"></div>`;
+let text = new Array(
+    "Welkom in mijn website.","Dit is de tweede tekst.","Dit is de 3e tekst, ja je weet toch.","Northface.","Joey bravo now out in cinemas.","Basis1 module4 opdracht4.","Banaan.","Appel.","Meloen.","Rood."
+    );
+let r = 50;
+let g = 50;
+let b = 50;
+for(let i = 0; i < amountOfDivs; i++){
+    let i2 = i + 1;
+    // r += 10.25;
+    g += 10.25;
+    b += 20.5;
+    document.body.innerHTML += `<div id="divInsideDiv${i}" style="background-color: rgb(${r},${g},${b});"><p>${text[i]}</p></div>`;
 }
