463fe53e8bf4f6face6bfe0cd39a802dbe354d9b
87243 <87243@glr.nl>
2022-01-19 14:56:48 +0100

    basis1 module5 opdracht3 finished

--- basis1/module5/opdracht3/js/script.js
+++ basis1/module5/opdracht3/js/script.js
@@ -1,5 +1,20 @@
-addEventListener(keyup, funkyTime());
+let sounds = ["sound0", "sound1", "sound2", "sound3"];
 
-function funkyTime(){
-    console.log("It works!")
-}
+document.body.addEventListener("keyup", function(event){
+    funkyTime(event.key);
+});
+
+function funkyTime(button){
+    if (button == "a" || button == "A"){
+        new Audio(`media/${sounds[0]}.mp3`).play();
+    };
+    if (button == "s" || button == "S"){
+        new Audio(`media/${sounds[1]}.mp3`).play();
+    };
+    if (button == "d" || button == "D"){
+        new Audio(`media/${sounds[2]}.mp3`).play();
+    };
+    if (button == "f" || button == "F"){
+        new Audio(`media/${sounds[3]}.mp3`).play();
+    };
+};
