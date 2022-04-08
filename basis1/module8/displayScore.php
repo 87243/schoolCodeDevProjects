60b8cb19e754d3455e342f8600a5865419f343de
87243 <87243@glr.nl>
2022-04-08 12:02:08 +0200

    basis1 module8 fix

--- basis1/module8/displayScore.php
+++ basis1/module8/displayScore.php
@@ -6,6 +6,6 @@
     $result = $db -> query($query);
 
     while($row = $result -> fetchArray(SQLITE3_ASSOC)){
-        echo "Name: ", $row['naam'], " - Score: ", $row['Score'], "\n";
+        echo "Name: ", $row['Naam'], " - Score: ", $row['Score'], "\n";
     }
 ?>
