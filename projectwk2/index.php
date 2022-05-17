234c3357c3cd1b3e2b671057643a154e59e61322
87243 <87243@glr.nl>
2022-05-17 02:22:26 +0200

    projectwk2 update

---
+++ projectwk2/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>GLR Projectwk2 D1A1 87243</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <!-- Navbar: -->
+        <nav>
+            <div>
+                <div><img src="media/logo.png"></div>
+                <div><h1>GLR | Hoe zouden de software developer lokalen er uit moeten zien?</h1></div>
+                <!-- <a href="contact">Contact</a>
+                <a href="villas">Villas</a>
+                <a href="">Home</a> -->
+            </div>
+        </nav>
+        <!-- End of navbar -->
+
+        <!-- Content: -->
+        <div id="contentDiv">
+            <h1>Wie ben ik?</h1>
+            <p>Ik ben Kewin, ik zit op het GLR school al bijna een jaar, ik zit op het opleiding softwaredeveloper.</p>
+
+            <h1>Hoe zou een thuisbasis er volgens mij uit moeten komen te zien?</h1>
+            <p>bruh</p>
+
+            <h1>Hoe moet een praktijklokaal eruit komen te zien?</h1>
+            <p>bruh</p>
+            <h2>Waarom ik voor dit concept gekozen heb?</h2>
+            <p>bruh</p>
+
+            <h1>Hoe moet een theorielokaal eruit komen te zien?</h1>
+            <p>bruh</p>
+            <h2>Waarom ik voor dit concept gekozen heb?</h2>
+            <p>bruh</p>
+
+            <h1>Hoe kan de gang wat leuker aangekleed worden?</h1>
+            <p>bruh</p>
+            <h2>Waarom ik voor dit concept gekozen heb?</h2>
+            <p>bruh</p>
+        </div>
+        <!-- End of content -->
+    </body>
+</html>
